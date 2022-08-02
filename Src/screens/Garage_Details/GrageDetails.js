5e1d148ecfe9ab291873a828eefdd8fc35136c05
Ashish kumar <108981165+Singhsabh336@users.noreply.github.com>
2022-08-02 18:18:34 +0530

    Add new functionality in GarageDetails Screen

--- Src/screens/Garage_Details/GrageDetails.js
+++ Src/screens/Garage_Details/GrageDetails.js
@@ -8,6 +8,7 @@ import {
   Image,
   ScrollView,
   TextInput,
+  Modal,
 } from 'react-native';
 import CustomButton from '../../components/Buttons/CustomButton';
 import {COLOR} from '../../utils/Colors';
@@ -20,7 +21,8 @@ const {height, width} = Dimensions.get('window');
 const GrageDetails = props => {
   const [check, setCheck] = useState(true);
   const [check1, setCheck1] = useState(true);
-  //  states for type of entity check
+  //  states for type of entity check Modal
+  const [modalVisible, setModalVisible] = useState(false);
 
   //  states for  No of KMP
 
@@ -319,6 +321,40 @@ const GrageDetails = props => {
     );
   };
 
+  const renderTextInput = () => {
+    return (
+      <View style={styles.nofParname}>
+        <View
+          style={{
+            height: height * 0.06,
+            width: width * 0.3,
+            borderWidth: 1,
+            justifyContent: 'center',
+            borderRadius: 10,
+            borderColor: 'rgb(225,225,225)',
+          }}>
+          <TextInput placeholder=" Enter Name" keyboardType="default" />
+        </View>
+        <View
+          style={{
+            height: height * 0.06,
+            width: width * 0.33,
+            justifyContent: 'center',
+            // alignItems: 'center',
+            borderWidth: 1,
+            borderRadius: 10,
+            borderColor: 'rgb(225,225,225)',
+          }}>
+          <TextInput
+            placeholder="Contect NO"
+            keyboardType="default"
+            maxLength={12}
+          />
+        </View>
+      </View>
+    );
+  };
+
   const renderEntityType = () => {
     return (
       <View style={styles.Entity}>
@@ -334,101 +370,124 @@ const GrageDetails = props => {
             </Text>
           </View>
 
-          <View style={styles.cheekk}>
-            <BouncyCheckbox
-              size={20}
-              fillColor="#02024A"
-              unfillColor="#FFFFFF"
-              text="Properitor Firm"
-              iconStyle={{
-                borderColor: 'rgb(245,245,245)',
-                borderRadius: 4,
-                borderColor: 'gray',
-              }}
-              // onPress={(isChecked: boolean) => {}}
-              textStyle={{
-                color: 'black',
-                textDecorationLine: 'none',
-                fontSize: height / 50,
-              }}
-            />
-          </View>
+          <TouchableOpacity
+            style={styles.cheekk}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text>Properitor Firm</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cheekk}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text>Partnership Firm</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cheekk}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text>LLP</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cheekk}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text>OPC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cheekk}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text>Company</Text>
+          </TouchableOpacity>
 
-          <View style={styles.cheekk}>
-            <BouncyCheckbox
-              size={20}
-              fillColor="#02024A"
-              unfillColor="#FFFFFF"
-              text=" Partnership Firm"
-              iconStyle={{
-                borderColor: 'rgb(245,245,245)',
-                borderRadius: 4,
-                borderColor: 'gray',
-              }}
-              // onPress={(isChecked: boolean) => {}}
-              textStyle={{
-                color: 'black',
-                textDecorationLine: 'none',
-                fontSize: height / 50,
-              }}
-            />
-          </View>
-          <View style={styles.cheekk}>
-            <BouncyCheckbox
-              size={20}
-              fillColor="#02024A"
-              unfillColor="#FFFFFF"
-              text="LLP"
-              iconStyle={{
-                borderColor: 'rgb(245,245,245)',
-                borderRadius: 4,
-                borderColor: 'gray',
-              }}
-              // onPress={(isChecked: boolean) => {}}
-              textStyle={{
-                color: 'black',
-                textDecorationLine: 'none',
-                fontSize: height / 50,
-              }}
-            />
-          </View>
-          <View style={styles.cheekk}>
-            <BouncyCheckbox
-              size={20}
-              fillColor="#02024A"
-              unfillColor="#FFFFFF"
-              text="OPC"
-              iconStyle={{
-                borderColor: 'rgb(245,245,245)',
-                borderRadius: 4,
-                borderColor: 'gray',
-              }}
-              // onPress={(isChecked: boolean) => {}}
-              textStyle={{
-                color: 'black',
-                textDecorationLine: 'none',
-                fontSize: height / 50,
-              }}
-            />
-          </View>
-          <View style={styles.cheekk}>
-            <BouncyCheckbox
-              size={20}
-              fillColor="#02024A"
-              unfillColor="#FFFFFF"
-              text="Company"
-              iconStyle={{
-                borderColor: 'rgb(245,245,245)',
-                borderRadius: 4,
-                borderColor: 'gray',
-              }}
-              // onPress={(isChecked: boolean) => {}}
-              textStyle={{
-                color: 'black',
-                textDecorationLine: 'none',
-                fontSize: height / 50,
-              }}
-            />
+          {/**========================================= Properitor FirmModal==================================================================== */}
+          <View style={styles.centeredView}>
+            <Modal
+              animationType="fade"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setModalVisible(!modalVisible);
+              }}>
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <View style={styles.nofPar}>
+                    <View
+                      style={{
+                        height: height * 0.05,
+                        width: width * 0.4,
+                        // borderWidth: 1,
+                        justifyContent: 'center',
+                      }}>
+                      <Text style={{color: 'black', fontSize: 14}}>
+                        Total No. of Partner
+                      </Text>
+                    </View>
+                    <View>
+                      <TextInput
+                        placeholder="0"
+                        placeholderTextColor={'red'}
+                        style={{
+                          height: height * 0.05,
+                          width: width * 0.14,
+                          borderWidth: 1,
+                          justifyContent: 'center',
+                          // alignItems: 'center',
+                          borderRadius: 10,
+                          paddingHorizontal: 15,
+                          borderColor: 'rgb(245,245,245)',
+                        }}
+                      />
+                    </View>
+                  </View>
+
+                  <View style={styles.nofParname}>
+                    <View
+                      style={{
+                        height: height * 0.06,
+                        width: width * 0.3,
+                        borderWidth: 1,
+                        justifyContent: 'center',
+                        borderRadius: 10,
+                        borderColor: 'rgb(225,225,225)',
+                      }}>
+                      <TextInput
+                        placeholder=" Enter Name"
+                        keyboardType="default"
+                      />
+                    </View>
+                    <View
+                      style={{
+                        height: height * 0.06,
+                        width: width * 0.33,
+                        justifyContent: 'center',
+                        // alignItems: 'center',
+                        borderWidth: 1,
+                        borderRadius: 10,
+                        borderColor: 'rgb(225,225,225)',
+                      }}>
+                      <TextInput
+                        placeholder="Contect NO"
+                        keyboardType="default"
+                        maxLength={12}
+                      />
+                    </View>
+                  </View>
+
+                  <TouchableOpacity
+                    style={{
+                      height: height * 0.05,
+                      width: width * 0.5,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      // borderWidth: 1,
+                      backgroundColor: 'rgb(225,225,225)',
+                      marginTop: 20,
+                      borderRadius: 20,
+                    }}
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={{fontSize: 16, color: 'black'}}>Submit</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
           </View>
         </View>
         <View style={styles.KmP}>
@@ -442,85 +501,6 @@ const GrageDetails = props => {
               No of KMP
             </Text>
           </View>
-
-          {/** 
-          <View style={styles.Enitycheck}>
-            <View style={styles.cheekk}>
-              <BouncyCheckbox
-                size={20}
-                fillColor="#02024A"
-                unfillColor="#FFFFFF"
-                text="NO"
-                iconStyle={{
-                  borderColor: 'rgb(245,245,245)',
-                  borderRadius: 4,
-                  borderColor: 'gray',
-                }}
-                // onPress={(isChecked: boolean) => {}}
-                textStyle={{color: 'black', textDecorationLine: 'none'}}
-              />
-            </View>
-
-            <View style={styles.cheekk}>
-              <BouncyCheckbox
-                size={20}
-                fillColor="#02024A"
-                unfillColor="#FFFFFF"
-                text="Total no of partner"
-                iconStyle={{
-                  borderColor: 'rgb(245,245,245)',
-                  borderRadius: 4,
-                  borderColor: 'gray',
-                }}
-                // onPress={(isChecked: boolean) => {}}
-                textStyle={{
-                  color: 'black',
-                  textDecorationLine: 'none',
-                  fontSize: height / 50,
-                }}
-              />
-            </View>
-
-            <View style={styles.cheekk}>
-              <BouncyCheckbox
-                size={20}
-                fillColor="#02024A"
-                unfillColor="#FFFFFF"
-                text="Total no of partner"
-                iconStyle={{
-                  borderColor: 'rgb(245,245,245)',
-                  borderRadius: 4,
-                  borderColor: 'gray',
-                }}
-                // onPress={(isChecked: boolean) => {}}
-                textStyle={{
-                  color: 'black',
-                  textDecorationLine: 'none',
-                  fontSize: height / 50,
-                }}
-              />
-            </View>
-
-            <View style={styles.cheekk}>
-              <BouncyCheckbox
-                size={20}
-                fillColor="#02024A"
-                unfillColor="#FFFFFF"
-                text=""
-                iconStyle={{
-                  borderColor: 'rgb(245,245,245)',
-                  borderRadius: 4,
-                  borderColor: 'gray',
-                }}
-                // onPress={(isChecked: boolean) => {}}
-                textStyle={{
-                  color: 'black',
-                  textDecorationLine: 'none',
-                  fontSize: height / 50,
-                }}
-              />
-            </View>
-          </View>*/}
         </View>
       </View>
     );
@@ -680,7 +660,7 @@ const styles = StyleSheet.create({
   // ========== entity ==========
 
   Entity: {
-    height: height * 0.35,
+    height: height * 0.45,
     width: width * 0.9,
     alignSelf: 'center',
     // borderWidth: 1,
@@ -700,14 +680,66 @@ const styles = StyleSheet.create({
   },
   cheekk: {
     height: height * 0.045,
-    width: width * 0.44,
-    // backgroundColor:'red',
+    width: width * 0.45,
+    backgroundColor: 'rgb(225,225,225)',
     justifyContent: 'center',
+    marginVertical: 5,
+    borderRadius: 8,
   },
   Nxtbtn: {
     height: height * 0.2,
     width: width * 0.9,
     // borderWidth: 1,
     alignSelf: 'center',
+  },
+
+  //============ Modal
+
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    height: height * 0.4,
+    width: width * 0.7,
+  },
+
+  cheekkModal: {
+    height: height * 0.045,
+    width: width * 0.45,
+    // backgroundColor: 'rgb(225,225,225)',
+    // justifyContent: 'center',
+    marginVertical: 5,
+    borderRadius: 8,
+  },
+  nofPar: {
+    height: height * 0.074,
+    width: width * 0.6,
+    // borderWidth: 1,
+    // justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  nofParname: {
+    height: height * 0.075,
+    width: width * 0.7,
+    // borderWidth: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
 });
