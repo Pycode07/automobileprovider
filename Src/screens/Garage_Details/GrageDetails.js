c7c6e66da6f170b4622a62c90282f8c03bb51c45
ranjeet kumar <ksbmranjeetkumar112@gmail.com>
2022-08-03 14:50:46 +0530

    validation

--- Src/screens/Garage_Details/GrageDetails.js
+++ Src/screens/Garage_Details/GrageDetails.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,21 +11,74 @@ import {
   Modal,
 } from 'react-native';
 import CustomButton from '../../components/Buttons/CustomButton';
-import {COLOR} from '../../utils/Colors';
-import {ImagePath} from '../../utils/ImagePath';
+import { COLOR } from '../../utils/Colors';
+import { ImagePath } from '../../utils/ImagePath';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-const {height, width} = Dimensions.get('window');
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+const { height, width } = Dimensions.get('window');
 
 const GrageDetails = props => {
   const [check, setCheck] = useState(true);
   const [check1, setCheck1] = useState(true);
+  const [value, setValue] = useState([])
+  const [form, setForm] = useState({
+    addres: '',
+    area: '',
+    city: '',
+    pinCode: '',
+    email: '',
+    website: '',
+    owned: false,
+    rented: false,
+    ownerName: '',
+  })
+  const [validation, setValidation] = useState({
+    addres: false,
+    city: false,
+    pinCode: false,
+    email: false,
+    garage: false,
+    Oname: false,
+    ownedRented: false,
+    file: false,
+  })
+  const email_validate = (text) => {
+    console.log(text);
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(text) === false) {
+      return false;
+    }
+    else {
+      return true
+    }
+  }
+  const check_validation = () => {
+    if (form.addres.length === 0) {
+      setValidation({ addres: true })
+    } else if (form.city.length === 0) {
+      setValidation({ city: true })
+    } else if (form.pinCode.length === 0) {
+      setValidation({ pinCode: true })
+    } else if (email_validate(form.email)) {
+      setValidation({ email: true })
+    } else if (form.owned === false && form.rented === false) {
+      setValidation({ ownedRented: true })
+    } else if (form.ownerName.length === 0) {
+      setValidation({ Oname: true })
+    } else {
+      return true
+    }
+  }
   //  states for type of entity check Modal
   const [modalVisible, setModalVisible] = useState(false);
-
+  const SaveData = () => {
+    if (check_validation()) {
+      props.navigation.navigate('Home')
+      console.log("Every thing fine.")
+    }
+  }
   //  states for  No of KMP
-
   const renderAddress = () => {
     return (
       <View style={styles.Address}>
@@ -54,9 +107,16 @@ const GrageDetails = props => {
             <TextInput
               placeholder="Full address"
               color={COLOR.TXT_INPT_COLOR}
+              value={form.addres}
+              onChangeText={(text) => { setForm({ addres: text }) }}
               placeholderTextColor={COLOR.GREY}
             />
           </View>
+          {
+            validation.addres ? <View style={{ marginBottom: 5 }}>
+              <Text style={{ color: 'red', fontSize: 13 }}>Please Enter correct Address...</Text>
+            </View> : null
+          }
         </View>
       </View>
     );
@@ -82,6 +142,8 @@ const GrageDetails = props => {
               placeholder="Enter Area"
               color={COLOR.TXT_INPT_COLOR}
               placeholderTextColor={COLOR.GREY}
+              value={form.area}
+              onChangeText={(text) => { setForm({ area: text }) }}
             />
           </View>
         </View>
@@ -102,6 +164,8 @@ const GrageDetails = props => {
               placeholder="City"
               color={COLOR.TXT_INPT_COLOR}
               placeholderTextColor={COLOR.GREY}
+              value={form.city}
+              onChangeText={(text) => { setForm({ city: text }) }}
             />
           </View>
         </View>
@@ -122,6 +186,8 @@ const GrageDetails = props => {
               placeholder="Pincode"
               color={COLOR.TXT_INPT_COLOR}
               placeholderTextColor={COLOR.GREY}
+              value={form.pinCode}
+              onChangeText={(text) => { setForm({ pinCode: text }) }}
             />
           </View>
         </View>
@@ -157,6 +223,8 @@ const GrageDetails = props => {
           }}>
           <View style={styles.gragecheck}>
             <BouncyCheckbox
+              isChecked={form.owned}
+              disableBuiltInState
               size={20}
               fillColor="#02024A"
               unfillColor="#FFFFFF"
@@ -166,7 +234,7 @@ const GrageDetails = props => {
                 borderRadius: 4,
                 borderColor: 'gray',
               }}
-              // onPress={(isChecked: boolean) => {}}
+              onPress={() => { setForm({ owned: true, rented: false }) }}
               textStyle={{
                 color: 'black',
                 textDecorationLine: 'none',
@@ -176,6 +244,8 @@ const GrageDetails = props => {
           </View>
           <View style={styles.gragecheck}>
             <BouncyCheckbox
+              isChecked={form.rented}
+              disableBuiltInState
               size={20}
               fillColor="#02024A"
               unfillColor="#FFFFFF"
@@ -185,7 +255,7 @@ const GrageDetails = props => {
                 borderRadius: 4,
                 borderColor: 'gray',
               }}
-              // onPress={(isChecked: boolean) => {}}
+              onPress={() => { setForm({ rented: true, owned: false }) }}
               textStyle={{
                 color: 'black',
                 textDecorationLine: 'none',
@@ -226,6 +296,8 @@ const GrageDetails = props => {
               placeholder="Full address"
               color={COLOR.TXT_INPT_COLOR}
               placeholderTextColor={COLOR.GREY}
+              value={form.email}
+              onChangeText={(text) => { setForm({ email: text }) }}
             />
           </View>
         </View>
@@ -255,6 +327,8 @@ const GrageDetails = props => {
               placeholder="Full address"
               color={COLOR.TXT_INPT_COLOR}
               placeholderTextColor={COLOR.GREY}
+              value={form.website}
+              onChangeText={(text) => { setForm({ website: text }) }}
             />
           </View>
         </View>
@@ -282,6 +356,8 @@ const GrageDetails = props => {
               placeholder="Enter name"
               color={COLOR.TXT_INPT_COLOR}
               placeholderTextColor={COLOR.GREY}
+              value={form.ownerName}
+              onChangeText={(text) => { setForm({ ownerName: text }) }}
             />
           </View>
         </View>
@@ -310,7 +386,7 @@ const GrageDetails = props => {
                 justifyContent: 'center',
                 alignItems: 'center',
               }}>
-              <Text style={{color: COLOR.BACK_BORDER, fontSize: 10}}>
+              <Text style={{ color: COLOR.BACK_BORDER, fontSize: 10 }}>
                 {' '}
                 Attach file
               </Text>
@@ -416,7 +492,7 @@ const GrageDetails = props => {
                         // borderWidth: 1,
                         justifyContent: 'center',
                       }}>
-                      <Text style={{color: 'black', fontSize: 14}}>
+                      <Text style={{ color: 'black', fontSize: 14 }}>
                         Total No. of Partner
                       </Text>
                     </View>
@@ -424,52 +500,69 @@ const GrageDetails = props => {
                       <TextInput
                         placeholder="0"
                         placeholderTextColor={'red'}
+                        keyboardType='numeric'
+                        value={value}
+                        onChangeText={(text) => {
+                          if(text>=3){
+                            text=3
+                          }
+                          let arr = []
+                          for (let i = 0; i < text; i++) {
+                            arr.push(i)
+                          }
+                          setValue(arr)
+                        }}
                         style={{
                           height: height * 0.05,
-                          width: width * 0.14,
+                          width: width * 0.1,
                           borderWidth: 1,
                           justifyContent: 'center',
+                          textAlign: 'center',
                           // alignItems: 'center',
                           borderRadius: 10,
-                          paddingHorizontal: 15,
+                          paddingVertical: 5,
                           borderColor: 'rgb(245,245,245)',
                         }}
                       />
                     </View>
                   </View>
+                  {
+                    value.map((i) => (
+                      <View style={styles.nofParname} key={i}>
+                        <View
+                          style={{
+                            height: height * 0.06,
+                            width: width * 0.3,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                            borderRadius: 10,
+                            borderColor: 'rgb(225,225,225)',
+                          }}>
+                          <TextInput
+                            placeholder=" Enter Name"
+                            keyboardType="default"
+                          />
+                        </View>
+                        <View
+                          style={{
+                            height: height * 0.06,
+                            width: width * 0.33,
+                            justifyContent: 'center',
+                            // alignItems: 'center',
+                            borderWidth: 1,
+                            borderRadius: 10,
+                            borderColor: 'rgb(225,225,225)',
+                          }}>
+                          <TextInput
+                            placeholder="Contect NO"
+                            keyboardType="default"
+                            maxLength={12}
+                          />
+                        </View>
+                      </View>
+                    ))
+                  }
 
-                  <View style={styles.nofParname}>
-                    <View
-                      style={{
-                        height: height * 0.06,
-                        width: width * 0.3,
-                        borderWidth: 1,
-                        justifyContent: 'center',
-                        borderRadius: 10,
-                        borderColor: 'rgb(225,225,225)',
-                      }}>
-                      <TextInput
-                        placeholder=" Enter Name"
-                        keyboardType="default"
-                      />
-                    </View>
-                    <View
-                      style={{
-                        height: height * 0.06,
-                        width: width * 0.33,
-                        justifyContent: 'center',
-                        // alignItems: 'center',
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        borderColor: 'rgb(225,225,225)',
-                      }}>
-                      <TextInput
-                        placeholder="Contect NO"
-                        keyboardType="default"
-                        maxLength={12}
-                      />
-                    </View>
-                  </View>
 
                   <TouchableOpacity
                     style={{
@@ -483,7 +576,7 @@ const GrageDetails = props => {
                       borderRadius: 20,
                     }}
                     onPress={() => setModalVisible(!modalVisible)}>
-                    <Text style={{fontSize: 16, color: 'black'}}>Submit</Text>
+                    <Text style={{ fontSize: 16, color: 'black' }}>Submit</Text>
                   </TouchableOpacity>
                 </View>
               </View>
@@ -507,19 +600,19 @@ const GrageDetails = props => {
   };
   // =============++ Main Function ++==========
   return (
-    <KeyboardAwareScrollView style={{flex: 1}}>
+    <KeyboardAwareScrollView style={{ flex: 1 }}>
       <View>
         <View style={styles.header}>
           <View style={styles.backbtn}>
             <TouchableOpacity onPress={() => props.navigation.goBack()}>
               <Image
                 source={ImagePath.BlACK_BACK_ARROW}
-                style={{height: 25, width: 25}}
+                style={{ height: 25, width: 25 }}
               />
             </TouchableOpacity>
           </View>
           <View style={styles.titaltxxt}>
-            <Text style={{color: COLOR.BLACK, fontSize: 21, fontWeight: '700'}}>
+            <Text style={{ color: COLOR.BLACK, fontSize: 21, fontWeight: '700' }}>
               Garage Details
             </Text>
           </View>
@@ -534,7 +627,7 @@ const GrageDetails = props => {
         <View style={styles.Nxtbtn}>
           <CustomButton
             title={'Next'}
-            ButtonPress={() => props.navigation.navigate('Home')}
+            ButtonPress={() => SaveData()}
           />
         </View>
       </View>
